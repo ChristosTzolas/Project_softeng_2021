d04431f05522b47b9cbea54b569adf52040f765d
lefokithas <lefteriskolokithas@hotmail.com>
2021-06-01 22:01:13 +0300

    Create file + user + admin
    created 2 classes

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class User {
+    int user_id;
+    String username;
+    String password;
+    String surname;
+    String name;
+    int phone;
+    String email;
+    Date birth_date;
+    String itineraries;
+}
+
+
+class Admin {
+    int adm_id;
+    String username;
+    String password;
+    String email;
+}
+
+
+
