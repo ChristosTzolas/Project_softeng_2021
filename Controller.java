89a2ed908010d3a6e7ae08c9a33c2988135e3946
ChristosTzolas <79763667+ChristosTzolas@users.noreply.github.com>
2021-06-05 00:31:28 +0300

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,1 +1,29 @@
+import javafx.event.ActionEvent;
 
+public class Controller {
+    public void show_calendar(ActionEvent event){
+      char event_holder;
+      int k;
+      get_user_events();
+      events = event_holder;
+      k = that_number(events); 
+      int i = 0;
+      while (i<k){
+         countdown();
+         System.out.println("I want to die");
+         k++;
+      }
+     }
+    public void countdown(ActionEvent event){
+      
+    }
+    
+}
+
+public void setUserevents
+return events;
+set_user_events(){
+  Events
+}
+get_user_events()
+public void setMyLife()
