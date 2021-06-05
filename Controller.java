bdc0195df67d7929e88b591de2e4475accef7362
ChristosTzolas <79763667+ChristosTzolas@users.noreply.github.com>
2021-06-05 13:47:05 +0300

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,6 +1,35 @@
 import javafx.event.ActionEvent;
 
 public class Controller {
+    
+    public void emergency_button(ActionEvent event){
+        char automated_message;
+        int i=0;
+        get_saved_numbers();
+        if (numbers =! null){
+            gps();
+            get_message();
+            message = automated_message;
+            while (response = false){
+                send_to_first_number();
+                // need to fix the timer here
+                // something with response
+            }
+        } 
+        else {
+                System.out.println("There are no saved numbers in this device");
+        }
+        send_sos_to_112();
+        get_active_volunteers();
+        if (volunteers =! null){
+            System.out.println("Available volunteers in the area: %something", volunteers);
+        }
+                
+    }
+    public void help_button(ActionEvent event){
+        
+    }
+    
     public void show_calendar(ActionEvent event){
       char event_holder;
       int k;
