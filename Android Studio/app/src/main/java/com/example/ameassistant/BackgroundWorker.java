992e1134a25ce85b6ce4f76788f1cdfd755b7022
Skoyntoyflis <50483190+Skoyntoyflis@users.noreply.github.com>
2021-06-11 21:55:13 +0300

    Final commits of Dim

--- Android Studio/app/src/main/java/com/example/ameassistant/BackgroundWorker.java
+++ Android Studio/app/src/main/java/com/example/ameassistant/BackgroundWorker.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.icu.util.Output;
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -32,8 +36,9 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
         String itinerary_url = "http://10.0.2.2/itinerary.php";
         if(type.equals("itinerary")){
             try {
+                String data[][];
                 String it_id = voids[1];
-                String it_vol_id = voids[2];
+//                String it_vol_id = voids[2];
                 URL url = new URL(itinerary_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
@@ -41,37 +46,54 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_data = URLEncoder.encode("it_id", "UTF-8")+"="+URLEncoder.encode(it_id, "UTF-8")+"&"+
-                        URLEncoder.encode("it_vol_id", "UTF-8")+"="+URLEncoder.encode(it_vol_id, "UTF-8");
+                String post_data = URLEncoder.encode("it_id", "UTF-8")+"="+URLEncoder.encode(it_id, "UTF-8");//+"&"+
+//                        URLEncoder.encode("it_vol_id", "UTF-8")+"="+URLEncoder.encode(it_vol_id, "UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 outputStream.close();
                 InputStream inputStream = httpURLConnection.getInputStream();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                StringBuilder stringBuilder = new StringBuilder();
                 String result="";
                 String line="";
                 while ((line = bufferedReader.readLine())!= null){
-                    result+=line;
+                    stringBuilder.append(line + "\n"); //result+=line;
                 }
+                result = stringBuilder.toString();
                 bufferedReader.close();
                 inputStream.close();
+                JSONArray jsonArray = new JSONArray(result);
+                JSONObject jsonObject = null;
+                data = new String[jsonArray.length()][];
+                for (int i=0; i<jsonArray.length(); i++){
+                    data[i] = new String[jsonArray.length()];
+                    jsonObject = jsonArray.getJSONObject(i);
+                    data[i][0] = jsonObject.getString("it_id"); // column name
+                    data[i][1] = jsonObject.getString("it_vol_id"); // column name
+                    data[i][2] = jsonObject.getString("date_and_time"); // column name
+                    data[i][3] = jsonObject.getString("locations"); // column name
+                    data[i][4] = jsonObject.getString("to_or_from"); // column name
+                    data[i][5] = jsonObject.getString("num_seats"); // column name
+                }
                 httpURLConnection.disconnect();
                 return result;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
 
         }
         else if(type.equals("createItinerary")){
             try {
-                String it_vol_id = voids[0];
-                String date_and_time = voids[1];
-                String location = voids[2];
-                String to_or_from = voids[3];
-                String num_seats = voids[4];
+//                String it_vol_id = voids[1];
+//                String date_and_time = voids[2];
+                String locations = voids[1];//voids[3];
+//                String to_or_from = voids[4];
+                String num_seats = voids[2];// voids[5];
                 URL url = new URL(create_itinerary_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
@@ -79,11 +101,12 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_data = URLEncoder.encode("it_vol_id", "UTF-8")+"="+URLEncoder.encode(it_vol_id, "UTF-8")+"&"+
-                        URLEncoder.encode("date_and_time", "UTF-8")+"="+URLEncoder.encode(date_and_time, "UTF-8")+"&"+
-                        URLEncoder.encode("location", "UTF-8")+"="+URLEncoder.encode(location, "UTF-8")+"&"+
-                        URLEncoder.encode("to_or_from", "UTF-8")+"="+URLEncoder.encode(to_or_from, "UTF-8")+"&"+
-                        URLEncoder.encode("num_seats", "UTF-8")+"="+URLEncoder.encode(num_seats, "UTF-8");
+                String post_data = URLEncoder.encode("locations", "UTF-8")+"="+URLEncoder.encode(locations, "UTF-8")+"&"+ URLEncoder.encode("num_seats", "UTF-8")+"="+URLEncoder.encode(num_seats, "UTF-8");
+//                String post_data = URLEncoder.encode("it_vol_id", "UTF-8")+"="+URLEncoder.encode(it_vol_id, "UTF-8")+"&"+
+//                        URLEncoder.encode("date_and_time", "UTF-8")+"="+URLEncoder.encode(date_and_time, "UTF-8")+"&"+
+//                        URLEncoder.encode("locations", "UTF-8")+"="+URLEncoder.encode(locations, "UTF-8")+"&"+
+//                        URLEncoder.encode("to_or_from", "UTF-8")+"="+URLEncoder.encode(to_or_from, "UTF-8")+"&"+
+//                        URLEncoder.encode("num_seats", "UTF-8")+"="+URLEncoder.encode(num_seats, "UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
@@ -122,7 +145,7 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
 //        AlertDialog alertDialog = alertDialogBuilder.create();
 //        alertDialog.show();
         alertDialog.setMessage(result);
-        alertDialog.show();
+//        alertDialog.show();
 //        this.statusField.setText("Login Successful");
 //        this.roleField.setText(result);
     }
--- Android Studio/app/src/main/java/com/example/ameassistant/ItemAdapter.java
+++ Android Studio/app/src/main/java/com/example/ameassistant/ItemAdapter.java
@@ -12,25 +12,31 @@ public class ItemAdapter extends BaseAdapter {
 
     LayoutInflater mInflater;
     ListView myListView ;
-    String[] items;
-    String[] prices;
-    String[] descriptions;
+    String[] it_ids;
+    String[] it_vol_ids;
+    String[] dates_and_times;
+    String[] locations;
+    String[] to_or_froms;
+    String[] nums_seats;
 
-    public ItemAdapter(Context c, String[] i, String[] p, String[] d){
-        items = i;
-        prices = p;
-        descriptions = d;
+    public ItemAdapter(Context c, String[] it_id, String[] it_vol_it, String[] date_and_time, String[] location, String[] to_or_from, String[] num_seats){
+        it_ids = it_id;
+        it_vol_ids = it_vol_it;
+        dates_and_times = date_and_time;
+        locations = location;
+        to_or_froms = to_or_from;
+        nums_seats = num_seats;
         mInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return items.length;
+        return it_ids.length;
     }
 
     @Override
     public Object getItem(int position) {
-        return items[position];
+        return it_ids[position];
     }
 
     @Override
@@ -42,17 +48,26 @@ public class ItemAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         View v = mInflater.inflate(R.layout.my_listview_detail, null);
-        TextView nameTextView = (TextView) v.findViewById(R.id.nameTextView);
-        TextView descriptionTextView = (TextView) v.findViewById(R.id.descriptionTextView);
-        TextView priceTextView = (TextView) v.findViewById(R.id.priceTextView);
+        TextView itIdListTextView = (TextView) v.findViewById(R.id.itIdListTextView);
+        TextView itVolIdListTextView = (TextView) v.findViewById(R.id.itVolIdListTextView);
+        TextView dateTimeListTextView = (TextView) v.findViewById(R.id.dateTimeListTextView);
+        TextView locationsListTextView = (TextView) v.findViewById(R.id.locationsListTextView);
+        TextView toOrFromListTextView = (TextView) v.findViewById(R.id.toOrFromListTextView);
+        TextView numSeatsListTextView = (TextView) v.findViewById(R.id.numSeatsListTextView);
 
-        String name = items[position];
-        String desc = descriptions[position];
-        String pric = prices[position];
+        String itid = it_ids[position];
+        String itvolid = it_vol_ids[position];
+        String datetime = dates_and_times[position];
+        String locat = locations[position];
+        String toorfrom = to_or_froms[position];
+        String numseats = nums_seats[position];
 
-        nameTextView.setText(name);
-        descriptionTextView.setText(desc);
-        priceTextView.setText(pric);
+        itIdListTextView.setText(itid);
+        itVolIdListTextView.setText(itvolid);
+        dateTimeListTextView.setText(datetime);
+        locationsListTextView.setText(locat);
+        toOrFromListTextView.setText(toorfrom);
+        numSeatsListTextView.setText(numseats);
 
 
 
